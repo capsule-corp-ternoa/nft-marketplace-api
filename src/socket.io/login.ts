887a85d9734d6ccbf2397e5788530baf2f128b7a
Cyril <cyril.caulonque@protonmail.com>
2021-04-12 17:29:03 +0200

    add msg to connection success event

--- src/socket.io/login.ts
+++ src/socket.io/login.ts
@@ -5,7 +5,9 @@ export default (io: Namespace) => {
     const { session } = socket.handshake.query;
 
     socket.join(session);
-    io.to(session).emit("CONNECTION_SUCCESS");
+    io.to(session).emit("CONNECTION_SUCCESS", {
+      msg: "connection successful",
+    });
 
     socket.on("SEND_WALLET_ID", ({ session: sessionArg, walletId }) => {
       socket.to(`${sessionArg}`).emit("RECEIVE_WALLET_ID", { walletId });
