db08d5d337b663d6a6becd55ce5e03b4f5fa1709
Maxime Lassalle <max@gama-smartweb.com>
2021-05-11 12:05:39 +0200

    cleanup

--- src/socket.io/login.ts
+++ src/socket.io/login.ts
@@ -5,9 +5,9 @@ import UserService from "../api/services/user";
 import L from "../common/logger";
 export default (io: Namespace) => {
   io.on("connection", async (socket: Socket) => {
-    const emitWalletId = async (_walletId: string, _session: string, callback: (args: any) => void | null = null) => {
+    const emitWalletId = async (walletId: string, _session: string, callback: (args: any) => void | null = null) => {
       const validCallback = callback && typeof callback === "function";
-      if (!_walletId) {
+      if (!walletId) {
         L.error(`Missing walletId argument for room ${_session}`);
         validCallback &&
           callback({ error: "400", msg: "Missing walletId argument" });
@@ -16,22 +16,22 @@ export default (io: Namespace) => {
         let user;
         // if user exists, retrieve it, otherwise create a new one, return error if it fails
         try {
-          user = await UserService.findUser(_walletId);
+          user = await UserService.findUser(walletId);
         } catch (err) {
           try {
-            user = await UserService.createUser({ walletId: _walletId });
+            user = await UserService.createUser({ walletId });
           } catch (err) {
             validCallback &&
               callback({ error: "500", msg: "Something went wrong" });
             return;
           }
         }
-        socket.to(`${_session}`).emit("RECEIVE_WALLET_ID", { walletId: _walletId });
+        socket.to(`${_session}`).emit("RECEIVE_WALLET_ID", { walletId });
         if (validCallback) callback({ ok: true });
       }
     }
 
-    const { session, walletId } = socket.handshake.query;
+    const { session } = socket.handshake.query;
 
     // if session arg not provided, return error and refuse connection
     if (!session || session === "undefined" || session === "") {
@@ -40,10 +40,8 @@ export default (io: Namespace) => {
       });
       socket.disconnect();
     } else {
-      io.adapter.once("join-room", (room, id) => {
-        socket.on("SEND_WALLET_ID", async ({ walletId: walltId }, callback) => {
-          emitWalletId(walltId, <string>session, callback);
-        });
+      socket.on("SEND_WALLET_ID", async ({ walletId }, callback) => {
+        emitWalletId(walletId, <string>session, callback);
       });
       await socket.join(session);
       io.to(socket.id).emit("CONNECTION_SUCCESS", {
