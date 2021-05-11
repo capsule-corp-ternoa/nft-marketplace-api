5c9f1a4127f419b0a61b713eba62918e80f8dc75
Maxime Lassalle <max@gama-smartweb.com>
2021-05-11 11:57:32 +0200

    socket counter fix console cleanup

--- src/socket.io/login.ts
+++ src/socket.io/login.ts
@@ -7,9 +7,8 @@ export default (io: Namespace) => {
   io.on("connection", async (socket: Socket) => {
     const emitWalletId = async (_walletId: string, _session: string, callback: (args: any) => void | null = null) => {
       const validCallback = callback && typeof callback === "function";
-      L.info(`emitWalletId?- session: ${_session} - walletId = ${_walletId}`);
       if (!_walletId) {
-        L.info(`Missing walletId argument for room ${_session}`);
+        L.error(`Missing walletId argument for room ${_session}`);
         validCallback &&
           callback({ error: "400", msg: "Missing walletId argument" });
       }
@@ -27,7 +26,6 @@ export default (io: Namespace) => {
             return;
           }
         }
-        L.info(`RECEIVE_WALLET_ID emitting for room ${_session} / walletId: ${_walletId}`);
         socket.to(`${_session}`).emit("RECEIVE_WALLET_ID", { walletId: _walletId });
         if (validCallback) callback({ ok: true });
       }
@@ -42,21 +40,13 @@ export default (io: Namespace) => {
       });
       socket.disconnect();
     } else {
-      L.info(`Login socket CONNECTED in room ${session} for id ${socket.id} at ${new Date()}`);
-      L.info(`Login socket JOIN room ${session} for id ${socket.id} at ${new Date()}`);
       io.adapter.once("join-room", (room, id) => {
-        L.info(`socket ${id} has joined room ${room}`);
         socket.on("SEND_WALLET_ID", async ({ walletId: walltId }, callback) => {
-          L.info(`emitWalletId given on event SEND_WALLET_ID`);
           emitWalletId(walltId, <string>session, callback);
         });
       });
-      io.adapter.once("leave-room", (room, id) => {
-        L.info(`socket ${id} has left room ${room}`);
-      });
       await socket.join(session);
       socket.on('disconnect', () => {
-        L.info(`Login socket DISCONNECTED in room ${session} for id ${socket.id} at ${new Date()}`);
       });
       io.to(socket.id).emit("CONNECTION_SUCCESS", {
         msg: "Connection successful",
