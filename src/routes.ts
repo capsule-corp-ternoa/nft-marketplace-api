ad905f1e542a14125e85cac42c774128a390c20f
Leouarz <ghalielouarzazi@hotmail.com>
2021-07-13 18:46:35 +0200

    add tiime route for get nfts in categories

--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,7 @@ import usersRouter from "./api/controllers/mpControllers/users/router";
 import nftsRouter from "./api/controllers/mpControllers/nfts/router";
 import categoriesRouter from "./api/controllers/mpControllers/categories/router";
 import appNftsRouter from "./api/controllers/appControllers/nfts/router"
+import tmNftsRouter from "./api/controllers/tmControllers/nfts/router"
 
 export default function routes(app: Application): void {
   // Marketplace routes
@@ -11,4 +12,7 @@ export default function routes(app: Application): void {
   app.use("/api/mp/categories", categoriesRouter);
   // Wallet app routes
   app.use("/api/app/NFTs", appNftsRouter)
+  // Tiime machin routes
+  app.use("/api/tm/NFTs", tmNftsRouter)
+
 }
