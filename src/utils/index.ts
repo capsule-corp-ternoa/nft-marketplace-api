a41bcf346e8142c1a5b7b60b8e9506fd327e659d
Maxime Lassalle <max@gama-smartweb.com>
2021-07-15 16:52:09 +0200

    fix abort controller import / ts usage

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,7 @@
 import fetch from 'node-fetch';
+import AbortController from "abort-controller"
 export const fetchTimeout = (url: string, options: any = null, timeoutLimit = 30000) => {
-    const controller = new AbortController()
+    const controller = new AbortController();
     const signal = controller.signal
     const timeout = setTimeout(() => {
         controller.abort()
