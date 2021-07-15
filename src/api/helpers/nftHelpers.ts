40b6a0b1630239e075a3242886774303630fa3f3
Maxime Lassalle <max@gama-smartweb.com>
2021-07-15 15:21:46 +0200

    NFT Uri fecth handled by a 5s timeout

--- src/api/helpers/nftHelpers.ts
+++ src/api/helpers/nftHelpers.ts
@@ -1,8 +1,8 @@
 import { ICompleteNFT, INFT } from "../../interfaces/graphQL";
 import UserService from "../services/user";
 import L from "../../common/logger";
-import fetch from "node-fetch";
 import NFTService from "../services/nft";
+import { fetchTimeout } from "src/utils";
 
 /**
  * Adds information to NFT object from external sources
@@ -61,8 +61,16 @@ export async function populateNFTOwner(
  */
 export async function populateNFTUri(NFT: INFT): Promise<ICompleteNFT | INFT> {
   try {
-    const info = await (await fetch(NFT.uri)).json();
-    return { ...NFT, ...info };
+    const response = await fetchTimeout(NFT.uri, 5000).catch((_e) => {
+      console.error('Could not retrieve NFT data from ' + NFT.uri);
+      throw new Error('Could not retrieve NFT data from ' + NFT.uri)
+    });
+    if (response) {
+      const info = await response.json();
+      return { ...NFT, ...info };
+    } else {
+      return null;
+    }
   } catch (err) {
     L.error({ err }, "invalid NFT uri");
     return NFT;
