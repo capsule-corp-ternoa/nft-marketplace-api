9e7e013131498b3bf895107be625f75f87a1eeeb
Waqas Ahmed <wa34992@gmail.com>
2021-07-20 14:29:08 +0500

    Changed variable neme to reviewRequested and removed extra code.

--- src/models/user.ts
+++ src/models/user.ts
@@ -37,6 +37,10 @@ const User = new mongoose.Schema({
     type: Boolean,
     default: false
   },
+  reviewRequested: {
+    type: Boolean,
+    default: false
+  },
   verified: {
     type: Boolean,
     default: false,
