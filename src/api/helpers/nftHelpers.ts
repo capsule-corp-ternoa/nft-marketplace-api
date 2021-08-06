ed0c52d96ee6b0e31ad3ecbd1f959a41259457ed
Maxime Lassalle <max@gama-smartweb.com>
2021-08-06 16:01:23 +0200

    console log removal

--- src/api/helpers/nftHelpers.ts
+++ src/api/helpers/nftHelpers.ts
@@ -119,7 +119,6 @@ export async function populateNFTOwner(
  */
 export async function populateNFTUri(NFT: INFT): Promise<any> {
   try {
-    L.info('Fetch attempt on NFT.uri:'+NFT.uri);
     const response = await fetchTimeout(NFT.uri, null, Number(process.env.IPFS_REQUEST_TIMEOUT) || 8000).catch((_e) => {
       L.error('fetch error:'+_e);
       throw new Error('Could not retrieve NFT data from ' + NFT.uri)
