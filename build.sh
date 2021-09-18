45f72bb20d521c3bf60d96fc8910c262accb3207
SOUFIANE BOUCHAARA <soufiane.devc@gmail.com>
2021-09-19 02:37:57 +0400

    Add build script

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -e
+set -x
+
+[ -z "$APP_HOME" ] && export APP_HOME=$(pwd)
+
+[ -z "$MONGODB_ADDON_URI" ] && export MONGODB_URI="$MONGODB_ADDON_URI"
+[ -z "$REDIS_URL" ] && export REDIS_URL="$REDIS_URL"
+
+npm install
+npm run build
