9124d5dd771d2f05144809e2f68286fbecdc64b9
Leouarz <ghalielouarzazi@hotmail.com>
2021-09-28 14:03:31 +0200

    Add sentry

--- src/common/server.ts
+++ src/common/server.ts
@@ -5,12 +5,31 @@ import { Server } from "socket.io";
 import cors from "cors";
 import os from "os";
 import L from "./logger";
-
 import errorHandler from "../api/middlewares/error.handler";
 import redis from 'redis';
 import { createAdapter } from "socket.io-redis";
+import * as Sentry from "@sentry/node"
+import * as Tracing from "@sentry/tracing"
 
 const app = express();
+
+if (process.env.SENTRY_DSN){
+  Sentry.init({
+    dsn: process.env.SENTRY_DSN,
+    integrations: [
+      // enable HTTP calls tracing
+      new Sentry.Integrations.Http({ tracing: true }),
+      // enable Express.js middleware tracing
+      new Tracing.Integrations.Express({
+        // to trace all requests to the default router
+        app,
+        // alternatively, you can specify the routes you want to trace:
+        // router: someRouter,
+      }),
+    ],
+    tracesSampleRate: 1.0,
+  });
+}
 
 export default class ExpressServer {
   constructor() {
@@ -40,7 +59,11 @@ export default class ExpressServer {
   }
 
   router(routes: (app: Application) => void): ExpressServer {
+    
+    if (process.env.SENTRY_DSN && process.env.NODE_ENV !== "development") app.use(Sentry.Handlers.requestHandler());
+    if (process.env.SENTRY_DSN && process.env.NODE_ENV !== "development") app.use(Sentry.Handlers.tracingHandler());
     routes(app);
+    if (process.env.SENTRY_DSN && process.env.NODE_ENV !== "development") app.use(Sentry.Handlers.errorHandler());
     app.use(errorHandler);
     return this;
   }
